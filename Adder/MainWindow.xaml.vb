b8f8d022ab7abe9f5c5f3863a1c6b20ca9cbe1d3
Wilhelm Payne <wbpayne@wilhelmpayne.com>
2023-11-07 20:56:12 -0500

    Add project files.

---
+++ Adder/MainWindow.xaml.vb
@@ -0,0 +1,10 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Try
+            TextBox3.Text = Convert.ToDouble(TextBox1.Text) + Convert.ToDouble(TextBox2.Text)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
